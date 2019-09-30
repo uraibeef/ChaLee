b893afeb6b4eda2e987de62a91b0f62344b0c324
uraibeef <uraibeef@gmail.com>
2019-10-01 02:33:22 +0700

    Finished

--- js/main.js
+++ js/main.js
@@ -115,3 +115,19 @@
 	});
 
 })(jQuery);
+
+function myFunction() {
+	var dots = document.getElementById("dots");
+	var moreText = document.getElementById("more");
+	var btnText = document.getElementById("myBtn");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "อ่านเพิ่มเติม"; 
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "แสดงน้อยลง"; 
+	  moreText.style.display = "inline";
+	}
+  }
